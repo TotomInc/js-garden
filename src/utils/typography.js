ebfb8752e878880940dfa4c7cb0eff03e86ffac8
Thomas Cazade <cazade.thomas@gmail.com>
2019-03-22 09:37:17 +0100

    feat(typography): add and use DeYoung theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography';
+import DeYoung from 'typography-theme-de-young';
 
-Wordpress2016.overrideThemeStyles = () => {
+DeYoung.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
@@ -9,15 +9,13 @@ Wordpress2016.overrideThemeStyles = () => {
   }
 }
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(DeYoung);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
   typography.injectStyles()
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
+export default typography;
